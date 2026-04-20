25b40df9cc6f52a6c1f4c6480a3b4b5fafd0c0ce
julian <julian@practicely.com>
2026-04-20 13:31:11 +0100

    formula: deskpulse 30

--- Formula/deskpulse.rb
+++ Formula/deskpulse.rb
@@ -1,9 +1,9 @@
 class Deskpulse < Formula
   desc "Work-presence helper agent for macOS"
   homepage "https://github.com/aresukio/homebrew-deskpulse"
-  url "https://github.com/aresukio/homebrew-deskpulse/releases/download/29/deskpulse-29-macos-arm64.tar.gz"
-  version "29"
-  sha256 "8b129b94d30a3d470657f2b834e5b137ac6713337e8ccbd32b9cfcc6e28ae864"
+  url "https://github.com/aresukio/homebrew-deskpulse/releases/download/30/deskpulse-30-macos-arm64.tar.gz"
+  version "30"
+  sha256 "300a1f26d5cbd9ab1f0c59e8db5bdf6a6c7d853af621134d37690078473c3741"
   license "MIT"
 
   depends_on :macos
